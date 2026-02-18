4acfe54c9d7b34867a3cb59f4ac051f9678c06f7
Reginald Moyo <regimoyo@gmail.com>
2026-02-18 01:46:10 +0100

    moved API key to env file for security reasons

--- src/Components/NewsItem.jsx
+++ src/Components/NewsItem.jsx
@@ -2,14 +2,18 @@ import image from '../Assets/rich-tervet-i8r3Kt2-mr8-unsplash.jpg'
 
 const NewsItem = ({title, description, src, url}) => {
   return (
-        <div className="card bg-dark text-light mb-3 d-inline-block my-3 mx-3 px-2 py-2" style={{maxWidth:"345px"}}>
-  <img src={src ? src : image} style={{height:"200px",width:"327px"}} className="card-img-top" alt="..." />
-  <div className="card-body">
-    <h5 className="card-title">{title.slice(0,50)}</h5>
-    <p className="card-text">{description ? description.slice(0,90) : "We are still fetching details for this new event. Stay tuned!"}</p>
-    <a href={url} className="btn btn-primary">Read More</a>
-  </div>
-</div>
+      <div className="card bg-dark text-light mb-3 d-inline-block my-3 mx-3 px-2 py-2" style={{maxWidth:"345px"}}>
+        <img 
+            src={src ? src : image} 
+            style={{height:"200px",width:"327px"}} 
+            className="card-img-top" alt="..." 
+          />
+        <div className="card-body">
+          <h5 className="card-title">{title.slice(0,50)}</h5>
+          <p className="card-text">{description ? description.slice(0,90) : "We are still fetching details for this new event. Stay tuned!"}</p>
+          <a href={url} className="btn btn-primary">Read More</a>
+        </div>
+    </div>
   )
 }
 
