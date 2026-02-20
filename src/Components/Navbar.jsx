b50a8bff81876d042efd56972a6102ba36999cb7
Reginald Moyo <regimoyo@gmail.com>
2026-02-20 01:12:33 +0100

    fix ESLint href accessibility issue

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = ({setCategory}) => {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#"><span className="badge bg-light text-dark fs-4">NewsApp</span></a>
+    <a className="navbar-brand" href="/"><span className="badge bg-light text-dark fs-4">NewsApp</span></a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
