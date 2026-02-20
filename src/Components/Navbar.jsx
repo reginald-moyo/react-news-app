c67dd5fc3c80759b87341322e1e5badd744e6126
Reginald Moyo <regimoyo@gmail.com>
2026-02-20 01:34:51 +0100

    solved another ESLint issue

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = ({setCategory}) => {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
   <div className="container-fluid">
-    <a className="navbar-brand" href="/"><span className="badge bg-light text-dark fs-4">NewsApp</span></a>
+    <button className="navbar-brand" ><span className="badge bg-light text-dark fs-4">NewsApp</span></button>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
